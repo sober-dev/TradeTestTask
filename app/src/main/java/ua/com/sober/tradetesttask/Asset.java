d2eebd215bb20c94d4d1b96cac1216381c2cfcca
Dmitry Hmel <dmitry.hmel@gmail.com>
2016-11-29 15:38:21 +0200

    ok parse result in JsoupUnitTest

--- app/src/main/java/ua/com/sober/tradetesttask/Asset.java
+++ app/src/main/java/ua/com/sober/tradetesttask/Asset.java
@@ -1,6 +1,5 @@
 package ua.com.sober.tradetesttask;
 
-import android.text.Spannable;
 
 /**
  * Created by Dmitry on 11/29/2016.
@@ -8,7 +7,62 @@ import android.text.Spannable;
 
 public class Asset {
     private String name;
-    private Spannable spannableCurrentRate;
+    private boolean isEnabled;
+    // direction == true if UP or false if DOWN
+    private boolean direction;
+    private String currentRateString;
     private float currentRate;
     private String change;
+
+    public Asset() {
+        this.currentRateString = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public boolean isDirection() {
+        return direction;
+    }
+
+    public void setDirection(boolean direction) {
+        this.direction = direction;
+    }
+
+    public String getCurrentRateString() {
+        return currentRateString;
+    }
+
+    public void setCurrentRateString(String currentRateString) {
+        this.currentRateString = currentRateString;
+    }
+
+    public float getCurrentRate() {
+        return currentRate;
+    }
+
+    public void setCurrentRate(float currentRate) {
+        this.currentRate = currentRate;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
 }
--- app/src/test/java/ua/com/sober/tradetesttask/JsoupUnitTest.java
+++ app/src/test/java/ua/com/sober/tradetesttask/JsoupUnitTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dmitry on 11/28/16.
@@ -18,29 +20,63 @@ public class JsoupUnitTest {
     @Test
     public void getDataFromTable() {
         Document document = null;
-//        Element table = null;
-        Elements assets = null;
+        Elements assetElements = null;
+        List<Asset> assetList = new ArrayList<>();
         try {
             document = Jsoup.connect("https://trade.opteck.com/trade/iframe?view=table").get();
-//            table = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t").first();
-            assets = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t > div.o-tr-line");
+            assetElements = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t > div.o-tr-line");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for (Element asset : assets) {
-            boolean isEnabled = asset.attributes().get("class").equals("o-tr-line active-pr-alert");
+        for (Element assetElement : assetElements) {
+            Asset asset = new Asset();
 
-            if (isEnabled) {
-                System.out.println("enabled: " + asset.attributes().get("class"));
-            } else {
-                System.out.println("disabled: " + asset.attributes().get("class"));
+            // Parse status
+            boolean isEnabled = assetElement.attributes().get("class").equals("o-tr-line active-pr-alert");
+            asset.setEnabled(isEnabled);
+
+            // Parse name
+            Element nameElement = assetElement.select("div.cell.instrument > div > span").first();
+            asset.setName(nameElement.text());
+
+            // Parse current rate
+            Element rateElement = assetElement.select("div.cell.cur-rate").first();
+            boolean isUp = rateElement.attributes().get("class").equals("cell cur-rate up");
+            asset.setDirection(isUp);
+            Elements rateStringElements = rateElement.select("div.item > div.rate-arrow > span");
+            String rateString = "";
+            for (Element rateStringElement : rateStringElements) {
+                switch (rateStringElement.attributes().get("class")) {
+                    case "arrow-range":
+                        break;
+                    case "":
+                        asset.setCurrentRateString(asset.getCurrentRateString() + rateStringElement.text());
+                        break;
+                    case "big":
+                        asset.setCurrentRateString(asset.getCurrentRateString() + "<big><b>" + rateStringElement.text() + "</b></big>");
+                        break;
+                    case "small":
+                        asset.setCurrentRateString(asset.getCurrentRateString() + "<sup>" + rateStringElement.text() + "</sup>");
+                        break;
+                    default:
+                        break;
+                }
+                if (!rateStringElement.text().equals(",")) {
+                    rateString += rateStringElement.text();
+                }
             }
+            asset.setCurrentRate(Float.parseFloat(rateString));
 
+            // Parse change
+            Element changeElement = assetElement.select("div.cell.change-td > div.item").first();
+            asset.setChange(changeElement.text());
+
+            assetList.add(asset);
         }
 
         assertNotNull(document);
-//        assertNotNull(table);
-        assertNotNull(assets);
+        assertNotNull(assetElements);
+        assertFalse(assetList.isEmpty());
     }
 }
