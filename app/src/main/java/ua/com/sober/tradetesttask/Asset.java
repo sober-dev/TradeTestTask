b91a56fb018f44cfab44cde54c79cf52026c908d
Dmitry Hmel <dmitry.hmel@gmail.com>
2016-11-29 02:41:57 +0200

    some test

---
+++ app/src/main/java/ua/com/sober/tradetesttask/Asset.java
@@ -0,0 +1,14 @@
+package ua.com.sober.tradetesttask;
+
+import android.text.Spannable;
+
+/**
+ * Created by Dmitry on 11/29/2016.
+ */
+
+public class Asset {
+    private String name;
+    private Spannable spannableCurrentRate;
+    private float currentRate;
+    private String change;
+}
--- app/src/test/java/ua/com/sober/tradetesttask/JsoupUnitTest.java
+++ app/src/test/java/ua/com/sober/tradetesttask/JsoupUnitTest.java
@@ -3,6 +3,7 @@ package ua.com.sober.tradetesttask;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -17,14 +18,29 @@ public class JsoupUnitTest {
     @Test
     public void getDataFromTable() {
         Document document = null;
-        Element table = null;
+//        Element table = null;
+        Elements assets = null;
         try {
             document = Jsoup.connect("https://trade.opteck.com/trade/iframe?view=table").get();
-            table = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t").first();
+//            table = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t").first();
+            assets = document.select("body > div.int-Panel.line.tableViewBO > div.line.table-box > div.ins-t-block.table > div.body-t > div.o-tr-line");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (Element asset : assets) {
+            boolean isEnabled = asset.attributes().get("class").equals("o-tr-line active-pr-alert");
+
+            if (isEnabled) {
+                System.out.println("enabled: " + asset.attributes().get("class"));
+            } else {
+                System.out.println("disabled: " + asset.attributes().get("class"));
+            }
+
+        }
+
         assertNotNull(document);
-        assertNotNull(table);
+//        assertNotNull(table);
+        assertNotNull(assets);
     }
 }
